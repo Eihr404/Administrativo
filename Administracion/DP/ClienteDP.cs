0147c2c128bde2be06d4e6abcbc10d074be39f13
DaniellaPR <pauladanielapozo@gmail.com>
2026-01-02 17:02:52 -0500

    Cambio en Properties

--- Administracion/DP/ClienteDP.cs
+++ Administracion/DP/ClienteDP.cs
@@ -5,21 +5,9 @@
         public string UsrNombre { get; set; } = string.Empty;                   
         public string CliCedula { get; set; } = string.Empty;                   
         public string Rol { get; set; } = string.Empty;
-        public string EstadoCodigo { get; set; } = string.Empty;
-        public string Estado
-        {
-            get
-            {
-                if (EstadoCodigo == "A")
-                {
-                    return "ACTIVO";
-                }
-                else
-                {
-                    return "INACTIVO";
-                }
-            }
-        }
+        public string Estado { get; set; } = string.Empty;
+        public string NombreFactura { get; set; } = string.Empty;
+        public string DireccionFactura { get; set; } = string.Empty;
 
     }
 }
